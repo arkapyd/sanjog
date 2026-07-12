436d8aa4a34ef6b7c9a8a601f88a4ed8d2bb2b0d
arka <hs24h052@smail.iitm.ac.in>
2026-07-12 18:40:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SANJOG service worker
-const CACHE = "sanjog-v7";
+const CACHE = "sanjog-v8";
 const SHELL = ["./", "./index.html", "./engine.js", "./network.json",
-               "./manifest.webmanifest",
+               "./manifest.webmanifest", "./bg-howrah-sketch.jpg",
                "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png"];
 
 self.addEventListener("install", e => {
